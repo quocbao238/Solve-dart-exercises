b9e5773bb42a5d10917b6986967a21ff9ba2c5e5
Quoc Bao Bui <baobondev@gmail.com>
2022-11-29 15:06:41 +0700

    Update pig_latin.dart

--- pig-latin/lib/pig_latin.dart
+++ pig-latin/lib/pig_latin.dart
@@ -1,52 +1,29 @@
-final regExpvowelChar = RegExp(r'^([aeiou]|yt|xr)');
-
 String translate(String val) {
-  val = val.toLowerCase();
-
-  final listWords = val.split(' ');
-
+  final listWords = val.toLowerCase().split(' ');
   //a whole phrase
   return listWords.map((e) => translateWord(e)).toList().join(" ");
 }
 
 String translateWord(String val) {
-  
-   //Aded to words that start with vowels'
-  // This rule will be true for the current test case
-  // If want to be more precise you can check with startsWith
-  if (isVowel(val)) return val + 'ay';
+  final _lastChar = 'ay';
+  if (isVowel(val)) return val + _lastChar;
 
-  // some letter clusters are treated like a single consonant
+  if (startWithThreeLetterCluster(val) || checkFollowed(val))
+    return val.substring(3, val.length) + val.substring(0, 3) + _lastChar;
 
-  if (startWithThreeLetterCluster(val)) {
-    return val.substring(3, val.length) + val.substring(0, 3) + 'ay';
-  }
+  if (startWitTwoLetterCluster(val))
+    return val.substring(2) + val.substring(0, 2) + _lastChar;
 
-  if (startWitTwoLetterCluster(val)) {
-    return val.substring(2) + val.substring(0, 2) + 'ay';
-  }
-
-  //word beginning with qu and a preceding consonant
-  String _afterConsonant = val.substring(1, 3);
-  if (_afterConsonant == 'qu') {
-    return val.substring(3, val.length) + val.substring(0, 3) + 'ay';
-  }
-
-  //first letter and ay are moved to the end of words that start with consonants
-  if (!isVowel(val)) {
-    return val.substring(1, val.length) + val[0] + 'ay';
-  }
-
-  while (!isVowel(val[0])) {
-    val += val[0];
-    val = val.substring(1, val.length);
-  }
-
-  return val + 'ay';
+  return val.substring(1, val.length) + val[0] + _lastChar;
 }
 
+final regExpvowelChar = RegExp(r'^([aeiou]|yt|xr)');
+
 bool isVowel(String val) => regExpvowelChar.hasMatch(val);
-// some letter clusters are treated like a single consonant
+
+//starts with a consonant sound followed by "qu"
+bool checkFollowed(String val) => val.length > 2 && val.substring(1, 3) == 'qu';
+
 bool startWithThreeLetterCluster(String val) =>
     val.length > 2 && ["thr", "sch"].contains(val.substring(0, 3));
 
